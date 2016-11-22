876051bf2c8da2e12fa588814e85f97127db7208
Roberto Lema <robertoslema@hotmail.com>
2016-11-22 16:01:12 -0200

    comm

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
     [TestClass]
-    public class UnitTestQuiron
+    public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+
+        public void Take()
         {
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5) select num;
+            int[] teste = { 5, 4, 1, 3, 9 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
+        }
+
+
+        public void Skip()
+
+        {
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+            int[]  teste ={ 1,3,9};
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
     }
 }
