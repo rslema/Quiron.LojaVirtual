b2daefb8fa9cb37bf3ef7216eb0c00e946e37831
Roberto Lema <robertoslema@hotmail.com>
2016-11-22 19:12:52 -0200

    comm

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,31 +1,77 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
+
+
 
 namespace Quiron.LojaVirtual.UnitTest
+
 {
     [TestClass]
-    public class UnitTest1
+public class UnitTest1
+{
+    [TestMethod]
+    public void Take()
     {
-        [TestMethod]
+        //O operador Take é usado para selecionar os primeiros n objetos de uma coleção.
 
-        public void Take()
-        {
-            int[] numeros = { 5, 4, 1, 3, 9, 8, 7, 2, 0 };
-            var resultado = from num in numeros.Take(5) select num;
-            int[] teste = { 5, 4, 1, 3, 9 };
-            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-        }
+        var resultado = from num in numeros.Take(5) select num;
 
+        int[] teste = { 5, 4, 1, 3, 9 };
 
-        public void Skip()
+        CollectionAssert.AreEqual(resultado.ToArray(), teste);
 
-        {
-            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-            var resultado = from num in numeros.Take(5).Skip(2) select num;
-            int[]  teste ={ 1,3,9};
-            CollectionAssert.AreEqual(resultado.ToArray(), teste);
-        }
     }
+
+
+    [TestMethod]
+    public void Skip()
+    {
+
+        //O operador Skip ignora o(s) primeiro(s) n objetos de uma coleção.
+
+        int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+        var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+        int[] teste = { 1, 3, 9 };
+
+        CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
+    }
+
+
+
+
+
+    [TestMethod]
+    public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+    {
+        //Arrange
+
+        HtmlHelper html = null;
+        Paginacao paginacao = new Paginacao
+        {
+            PaginaAtual = 2,
+            ItensPorPagina = 5,
+            ItensTotal = 10
+        };
+        Func<int, string> paginaUrl = i => "Pagina" + i;
+        //Act
+        MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+        //Assert
+        Assert.AreEqual(
+
+            @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+            + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+
+            );
+    }
+}
 }
